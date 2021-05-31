463e314b4298a57761463297d99960eba36b9801
RegianeMachado <regi.sabbath@gmail.com>
2021-05-31 08:40:25 -0300

    Implementando JPA

--- src/main/java/com/algaworks/algalog/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/controller/ClienteController.java
@@ -1,29 +1,21 @@
 package com.algaworks.algalog.controller;
 
 import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 public class ClienteController {
 
+    private ClienteRepository clienteRepository;
+
     @GetMapping("/clientes")
     public List<Cliente> listar() {
-        Cliente cliente1 = new Cliente();
-        cliente1.setId(1L);
-        cliente1.setNome("Regi");
-        cliente1.setEmail("regi.sabbath@gmail.com");
-        cliente1.setTelefone("909080808");
-
-        Cliente cliente2 = new Cliente();
-        cliente2.setId(2L);
-        cliente2.setNome("Lien");
-        cliente2.setEmail("lien.sabbath@gmail.com");
-        cliente2.setTelefone("909080808");
-
-        return Arrays.asList(cliente1, cliente2);
+        return clienteRepository.findAll();
     }
 }
